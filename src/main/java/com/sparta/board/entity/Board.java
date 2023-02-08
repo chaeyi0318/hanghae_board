802a2f3bb7486cced83682a15a0e79590e40f2c1
Son-Chaeyi <chaeyi0318@gmail.com>
2023-02-08 16:31:15 +0900

    날짜 null값 수정 완료

--- src/main/java/com/sparta/board/entity/Board.java
+++ src/main/java/com/sparta/board/entity/Board.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class Board extends Timestamped {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
